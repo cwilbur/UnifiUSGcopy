076136a0e0a707b5bbc987d0f0fa1bcb3a478e05
rickmur <rick@mur.nl>
2016-06-26 14:19:36 +0200

    Update SSH details

--- USGcopy.py
+++ USGcopy.py
@@ -19,35 +19,35 @@ print ("-----------------------------------------")
 print ("Maverick.Solutions - Unifi USG Copy tool")
 print ("-----------------------------------------")
 
-# try:
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-log = logging.getLogger("USGcopy")
+try:
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+    log = logging.getLogger("USGcopy")
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(HOST, SSHPORT, username=USERNAME, password=PASSWORD)
-sftpClient = ssh.open_sftp()
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(HOST, SSHPORT, username=USERNAME, password=PASSWORD)
+    sftpClient = ssh.open_sftp()
 
-if RUNNING_ON.lower() == "usg":
-    myConfig = check_output(["mca-ctrl", "-t dump-cfg"])
-    # DEBUG myConfig = check_output(["cat", "config.gateway.json"])
+    if RUNNING_ON.lower() == "usg":
+        myConfig = check_output(["mca-ctrl", "-t dump-cfg"])
+        # DEBUG myConfig = check_output(["cat", "config.gateway.json"])
 
-    sftpClient.put(os.path.realpath(os.path.dirname(__file__)) + "/" + USGFILE, CONTROLLERFILE)
-    log.info("File copied to controller")
-elif RUNNING_ON.lower() == "controller":
-    stdin, stdout, stderr = ssh.exec_command("mca-ctrl -t dump-cfg > config.gateway.json")
-    sftpClient.get(USGFILE, CONTROLLERFILE)
-    log.info("File copied from usg")
-else:
-    raise Exception("RUNNING_ON variable is not set to USG or Controller")
+        sftpClient.put(os.path.realpath(os.path.dirname(__file__)) + "/" + USGFILE, CONTROLLERFILE)
+        log.info("File copied to controller")
+    elif RUNNING_ON.lower() == "controller":
+        stdin, stdout, stderr = ssh.exec_command("mca-ctrl -t dump-cfg > config.gateway.json")
+        sftpClient.get(USGFILE, CONTROLLERFILE)
+        log.info("File copied from usg")
+    else:
+        raise Exception("RUNNING_ON variable is not set to USG or Controller")
 
-sftpClient.close()
-ssh.close()
-log.info("All done! Thank you!")
+    sftpClient.close()
+    ssh.close()
+    log.info("All done! Thank you!")
 
-# except Exception as e:
-#    msg = "Something went wrong: " + str(e.message)
-#    log.error(msg)
+except Exception as e:
+    msg = "Something went wrong: " + str(e.message)
+    log.error(msg)
 
 
 
